319f425274c18259a8cd5c5ede3e1081654815ae
Cabral <rodrigo.c.cabral@accenture.com>
2020-05-02 00:02:08 -0300

    02.05.2020 00:02

--- index.js
+++ index.js
@@ -1,33 +1,6 @@
-var slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+  /*Hamburger*/
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-
-}
-
-
- /*Hamburger*/
  function myFunction(x) {
   var classe1 = document.getElementsByClassName('container change');
 
@@ -40,3 +13,5 @@ function showSlides(n) {
 }
 
 }
+
+
